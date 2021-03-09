df85a34c4f9436caff4048bd9080e9d07c1bd54a
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 00:56:03 +0100

    Registered note_post api function to urls.py

--- note/urls.py
+++ note/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import note_list
+from .views import note_list, note_post
 
 urlpatterns = [
     path('', note_list, name="note-list"),
+    path('post/', note_post, name="note-post"),
 ]
