8996994212186e4464d08ddc8ce3f13822beea40
Israel Abraham <israelvictory87@gmail.com>
2021-03-09 22:48:06 +0100

    Imported rest_framework

--- note/views.py
+++ note/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Note
+from .serializers import NoteSerializer
 
+# Rest Framework Imports
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def note_list(request):
-    pass
+    note = Note.objects.all()
+    serializer = NoteSerializer(note, many=True)
+    return Response(serializer.data)
