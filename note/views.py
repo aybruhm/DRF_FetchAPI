4701d9dda6e9752dbd6867045005425d11fb7e06
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 00:55:42 +0100

    Created note_post api function

--- note/views.py
+++ note/views.py
@@ -5,9 +5,26 @@ from .serializers import NoteSerializer
 # Rest Framework Imports
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+
 
 @api_view(['GET'])
 def note_list(request):
     note = Note.objects.all()
     serializer = NoteSerializer(note, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def note_post(request):
+    if request.method == "GET":
+        note = Note.objects.all()
+        serializer = NoteSerializer(note, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
