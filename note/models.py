0963ce7623fc068f915d93d7808c9b51cbed9b0b
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 01:52:45 +0100

    Change Meta options on note model && Add field timestamp to note model

--- note/models.py
+++ note/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     thought = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ''
 
     def __str__(self):
         return self.author.username
