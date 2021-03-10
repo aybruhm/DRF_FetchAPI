9092149033616fdfed13c7acba21d2d1d2089ea5
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 02:14:17 +0100

    Created note_update api function

--- note/views.py
+++ note/views.py
@@ -28,3 +28,24 @@ def note_post(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def note_update(request, pk):
+    if request.method == "GET":
+        note = Note.objects.all()
+        serializer = NoteSerializer(note, many=True)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        try:
+            snippet = Note.objects.get(id=pk)
+        except Snippet.DoesNotExit:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # Put workflow
+        serializer = NoteSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
