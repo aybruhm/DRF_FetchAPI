93f9822f37247d22a0a0ce5dcaee0aacc914576c
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 01:53:08 +0100

    Created NoteAdmin class in admin.py

--- note/admin.py
+++ note/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Note
 
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'thought']
+
+
+admin.site.register(Note, NoteAdmin)
