bb0a4cccacf20cb371bf4630dd3ceb382a04a04f
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 15:35:54 +0100

    Created note_detail and note_delete api function

--- note/views.py
+++ note/views.py
@@ -5,47 +5,62 @@ from .serializers import NoteSerializer
 # Rest Framework Imports
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 
+@api_view(["GET"])
+def note_overview(request, format=None):
+    note_urls = {
+        'Get Notes': 'note/',
+        'Create Note': 'note/post/',
+        'Detail Note': 'note/<int:pk>/',
+        'Update Note': 'note/update/<int:pk>/',
+        'Delete Note': 'note/delete/<int:pk>/',
+    }
+    return Response(note_urls)
 
-@api_view(['GET'])
-def note_list(request):
+
+@api_view(["GET"])
+def note_list(request, format=None):
     note = Note.objects.all()
     serializer = NoteSerializer(note, many=True)
     return Response(serializer.data)
 
 
-@api_view(['GET', 'POST'])
-def note_post(request):
-    if request.method == "GET":
-        note = Note.objects.all()
-        serializer = NoteSerializer(note, many=True)
+@api_view(["GET"])
+def note_detail(request, pk, format=None):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def note_post(request, format=None):
+    serializer = NoteSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+def note_update(request, pk, format=None):
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExit:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = NoteSerializer(instance=note, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
         return Response(serializer.data)
-
-    elif request.method == "POST":
-        serializer = NoteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT'])
-def note_update(request, pk):
-    if request.method == "GET":
-        note = Note.objects.all()
-        serializer = NoteSerializer(note, many=True)
-        return Response(serializer.data)
-
-    if request.method == "PUT":
-        try:
-            snippet = Note.objects.get(id=pk)
-        except Snippet.DoesNotExit:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        # Put workflow
-        serializer = NoteSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+@api_view(["DELETE"])
+def note_delete(request, pk, format=None):
+    try:
+        note = Note.objects.get(id=pk)
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Note.DoesNotExit:
+        return Response(status=status.HTTP_404_NOT_FOUND)
