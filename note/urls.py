7de69c097ef4f8b55f36db00d39538442b07273b
Israel Abraham <israelvictory87@gmail.com>
2021-03-10 02:14:41 +0100

    Registered note_update api function to urls.py

--- note/urls.py
+++ note/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import note_list, note_post
+from .views import note_list, note_post, note_update
 
 urlpatterns = [
     path('', note_list, name="note-list"),
     path('post/', note_post, name="note-post"),
+    path('update/<int:pk>/', note_update, name="note-update"),
 ]
