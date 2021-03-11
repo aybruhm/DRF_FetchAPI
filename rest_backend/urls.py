a13dfd1e8b9b401343eb847a433bcf35b9882f98
Israel Abraham <israelvictory87@gmail.com>
2021-03-11 21:50:07 +0100

    Registered note_frontend to rest_backend urls.py

--- rest_backend/urls.py
+++ rest_backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('note/', include('note.urls')),
+    path('', include('note_frontend.urls')),
 ]
