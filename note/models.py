09c1a3c33a67d68ec04ef8d81bf99fa38b14d82a
Israel Abraham <israelvictory87@gmail.com>
2021-03-12 01:04:44 +0100

    Made new migrations

--- note/models.py
+++ note/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Note(models.Model):
+    title = models.CharField(max_length=100)
     thought = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
