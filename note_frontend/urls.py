35dbadb7b7cc0a36369b9b7666fed32bf71a322c
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 21:51:27 +0100

    Create url path for respective views

--- note_frontend/urls.py
+++ note_frontend/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import home
+from .views import home, create_thought, update_thought, \
+    delete_thought, login, register, profile
 
 urlpatterns = [
     path('', home, name="home"),
+    path('new-thought/', create_thought, name="new-thought"),
+    path('update-thought/', update_thought, name="update-thought"),
+    path('delete-thought/', delete_thought, name="delete-thought"),
+    path('login/', login, name="login"),
+    path('register/', register, name="register"),
+    path('profile/', profile, name="profile"),
 ]
