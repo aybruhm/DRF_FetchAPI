3b7600bf475ef26a626974e258ea259c10fe097e
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 21:51:49 +0100

    Initialize frontend_note views

--- note_frontend/views.py
+++ note_frontend/views.py
@@ -2,4 +2,28 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "note_frontend/index.html")
+    return render(request, "note_frontend/home.html")
+
+
+def create_thought(request):
+    return render(request, "note_frontend/write-thought.html")
+
+
+def update_thought(request):
+    return render(request, "note_frontend/edit-thought.html")
+
+
+def delete_thought(request):
+    return render(request, "note_frontend/confirm-delete.html")
+
+
+def login(request):
+    return render(request, "note_frontend/login.html")
+
+
+def register(request):
+    return render(request, "note_frontend/register.html")
+
+
+def profile(request):
+    return render(request, "note_frontend/profile.html")
