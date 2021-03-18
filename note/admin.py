af83742f9e3d1afffadbb2d12ac8158e29423bab
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 22:04:41 +0100

    Modified note_frontend base template

--- note/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ['id', 'author', 'thought']
-
-
-admin.site.register(Note, NoteAdmin)
