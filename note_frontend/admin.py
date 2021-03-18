b5b4cd3e91d62c81b1c910a3eb71d5151f3889c6
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 22:15:34 +0100

    Update note template files

--- note_frontend/admin.py
+++ note_frontend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title',)
+
+
+admin.site.register(Note, NoteAdmin)
