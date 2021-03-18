62f8b8f506eb7cb3f7ac841f2ade73e7fa2d6242
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 23:09:39 +0100

    Created url routes for respective api view

--- note_restapi/urls.py
+++ note_restapi/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import note_list, note_detail
 
 urlpatterns = [
+    path('', note_list, name="note-list"),
+    path('<int:pk>/', note_detail, name="note-detail"),
 ]
