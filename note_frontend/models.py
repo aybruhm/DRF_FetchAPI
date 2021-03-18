ef826e2d9df873c4bd7261991a250e58105b11b6
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 22:04:27 +0100

    Added the note db structures

--- note_frontend/models.py
+++ note_frontend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    thought = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return self.author.username
