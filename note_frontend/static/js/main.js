06a70e87cf5474e355d79d44eb797323e348ce5f
Israel Abraham <israelvictory87@gmail.com>
2021-03-18 21:43:42 +0100

    Import note app static files

--- note_frontend/static/js/main.js
+++ note_frontend/static/js/main.js
@@ -1,11 +1,5 @@
-// Fetch data from DRF backend
-
-var request = new Request('http://127.0.0.1:8000/note/');
-
-
-fetch(request).then(res => {
-    return res.json()
+fetch('http://127.0.0.1:8000/note/', {
+    mode: "no-cors"
+}).then((res) => {
+    console.log(res)
 })
-.then(data => {
-    console.log(data)
-});
