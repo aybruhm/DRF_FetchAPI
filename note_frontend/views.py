778a32228bcb0c653c677abf961a40ade7149904
Israel Abraham <israelvictory87@gmail.com>
2021-03-20 07:09:39 +0100

    Update update_thought view

--- note_frontend/views.py
+++ note_frontend/views.py
@@ -13,12 +13,20 @@ def home(request):
 
 def create_thought(request):
     form = NoteCreateForm()
+    print(request.user)
 
     if request.method == "POST":
         
         form = NoteCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            title = form.cleaned_data["title"]
+            thought = form.cleaned_data["thought"]
+            note = Note(
+                title=title,
+                thought=thought,
+                author=request.user,
+            )
+            note.save()
             return redirect("note:home")
         return redirect("note:new-thought")
 
@@ -28,11 +36,20 @@ def create_thought(request):
     return render(request, "note_frontend/write-thought.html", context)
 
 
-def update_thought(request):
+def update_thought(request, pk):
+    try:
+        note = Note.objects.get(id=pk)
+    except Note.DoesNotExist:
+        return redirect("note:home")
+
+    # TODO:
+    # - import update_thought form
+    # - implement updat/post method
+    # - uhh?
     return render(request, "note_frontend/edit-thought.html")
 
 
-def delete_thought(request):
+def delete_thought(request, pk):
     return render(request, "note_frontend/confirm-delete.html")
 
 
