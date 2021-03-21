a46644dbc331b61e8d28853e3123723bb7b65a5b
Israel Abraham <israelvictory87@gmail.com>
2021-03-21 21:20:26 +0100

    Created the class CreateUserForm (inherited UserCreationForm)

--- note_frontend/forms.py
+++ note_frontend/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Note
 
 
@@ -49,3 +51,34 @@ class NoteUpdateForm(forms.ModelForm):
                 }
             )
         }
+
+
+class CreateUserForm(UserCreationForm):
+    """
+    This class inherits from the base class *UserCreationForm*,
+    and creates a new user.
+    """
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        widgets = {
+        'username': forms.TextInput(attrs={
+            'class': 'form-control user_input',
+            'id': 'validationCustomUsername',
+            'aria-describedby': 'inputGroupPrepend',
+            'required': 'true',
+            }
+        ),
+        'password1': forms.PasswordInput(attrs={
+            'class': 'form-control password_input',
+            'id': 'validationCustom03',
+            'required': 'true',
+            }
+        ),
+        'password2': forms.PasswordInput(attrs={
+            'class': 'form-control password_input',
+            'id': 'validationCustom03',
+            'required': 'true',
+            }
+        ),
+    }
