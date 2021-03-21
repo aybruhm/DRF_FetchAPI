8205223b06b56e521ad1712a505853b7a10122a4
Israel Abraham <israelvictory87@gmail.com>
2021-03-22 00:06:48 +0100

    Modified login path

--- note_frontend/urls.py
+++ note_frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import home, create_thought, update_thought, \
-    delete_thought, login, register, profile
+    delete_thought, login_page, register, profile
 
 app_name = "note"
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('new-thought/', create_thought, name="new-thought"),
     path('update-thought/<int:pk>/', update_thought, name="update-thought"),
     path('delete-thought/<int:pk>/', delete_thought, name="delete-thought"),
-    path('login/', login, name="login"),
+    path('login/', login_page, name="login"),
     path('register/', register, name="register"),
     path('profile/', profile, name="profile"),
 ]
