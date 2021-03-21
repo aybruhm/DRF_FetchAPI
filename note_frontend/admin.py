ebd537d115c8055e5aa3bcf0002d355f04ea7ebf
Israel Abraham <israelvictory87@gmail.com>
2021-03-22 00:14:08 +0100

    Registered Profile and ProfileAdmin to admin site

--- note_frontend/admin.py
+++ note_frontend/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, Profile
 
 
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'title',)
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bio')
+
+
 admin.site.register(Note, NoteAdmin)
+admin.site.register(Profile, ProfileAdmin)
