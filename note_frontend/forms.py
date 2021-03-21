d9362e52601a6b28e60ee0dfb2be02dffad740de
Israel Abraham <israelvictory87@gmail.com>
2021-03-21 21:58:20 +0100

    Create LogUserForm

--- note_frontend/forms.py
+++ note_frontend/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,\
+    AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
 from .models import Note
 
@@ -90,3 +91,30 @@ class CreateUserForm(UserCreationForm):
             }
         ),
     }
+
+
+class LogUserForm(AuthenticationForm):
+    """
+    A form that inherits from the base *AuthenticationForm*,
+    and logs a user, with no privileges, from the given 
+    username and password.
+    """
+    username = UsernameField(
+        widget=forms.TextInput(attrs={
+            'autocomplete': 'username',
+            'class': 'form-control user_input',
+            'id': 'validationCustomUsername',
+            'aria-describedby': 'inputGroupPrepend',
+            'required': 'true',
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'autocomplete': 'new-password',
+            'class': 'form-control password_input',
+            'id': 'confirmPassword1',
+            'required': 'true',
+            }
+        )
+    )
