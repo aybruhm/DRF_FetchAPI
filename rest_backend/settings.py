6e4cca560bc596ba41a79023f31dbdcf59a90cc6
Israel Abraham <israelvictory87@gmail.com>
2021-03-22 13:02:55 +0100

    Update settings.py

--- rest_backend/settings.py
+++ rest_backend/settings.py
@@ -127,8 +127,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/login/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [
+    
+]
