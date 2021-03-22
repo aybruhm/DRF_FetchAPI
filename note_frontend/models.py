fc0cee45dbf0740a9f5e6cdc69f0527a1fb66440
Israel Abraham <israelvictory87@gmail.com>
2021-03-22 21:29:20 +0100

    Import signals and wrote a Profile trigger logic

--- note_frontend/models.py
+++ note_frontend/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+# from PIL import Image
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
     thought = models.TextField()
@@ -34,3 +38,23 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+      # Resize User Profile Picture 
+    # def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    #     super().save()
+    #     profile_picture = Image.open(self.profile_picture.path)
+    #     if profile_picture.height > 300 or profile_picture.width > 300:
+    #         output_size = (300, 300)
+    #         profile_picture.thumbnail(output_size)
+    #     print(profile_picture)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
