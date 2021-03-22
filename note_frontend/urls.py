7b078750b1c791ed945bb980c4e7ff09f37f49f8
Israel Abraham <israelvictory87@gmail.com>
2021-03-22 12:46:25 +0100

    Created url path for confirm_delete view

--- note_frontend/urls.py
+++ note_frontend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home, create_thought, update_thought, \
-    delete_thought, login_page, register_page, profile_page
+    delete_thought, login_page, register_page, profile_page, \
+        confirm_delete
 
 app_name = "note"
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path('', home, name="home"),
     path('new-thought/', create_thought, name="new-thought"),
     path('update-thought/<int:pk>/', update_thought, name="update-thought"),
+    path('confirm-delete/<int:pk>/', confirm_delete, name="confirm"),
     path('delete-thought/<int:pk>/', delete_thought, name="delete-thought"),
     path('login/', login_page, name="login"),
     path('register/', register_page, name="register"),
